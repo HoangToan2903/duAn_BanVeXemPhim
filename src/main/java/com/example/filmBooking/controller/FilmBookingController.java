233cba193ccef4586827c9315648d05e072cc00a
DESKTOP-JC8STTK\admin <toanhd290803@gmail.com>
2024-01-13 21:20:56 +0700

    upload file

---
+++ src/main/java/com/example/filmBooking/controller/FilmBookingController.java
@@ -0,0 +1,200 @@
+package com.example.filmBooking.controller;
+
+import com.example.filmBooking.model.*;
+import com.example.filmBooking.model.dto.DtoMovie;
+import com.example.filmBooking.repository.*;
+import com.example.filmBooking.service.impl.*;
+import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.util.Strings;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+@Controller
+@RequestMapping("/filmbooking")
+@SessionAttributes("soldTicketsCountBill")
+public class FilmBookingController {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private MovieServiceImpl service;
+
+    @Autowired
+    private CinemaServiceImpl cinemaService;
+
+    @Autowired
+    private ScheduleRepository repository1;
+
+    @Autowired
+    private BillRepository billRepository;
+
+    @Autowired
+    private RankCustomerServiceImpl rankCustomerService;
+
+    @Autowired
+    private GeneralSettingRepository generalSettingRepository;
+
+    @GetMapping("/trangchu")
+    public String getAllPosts(Model model, HttpServletRequest request) {
+//        String soldTicketsCount = billRepository.countSoldTicketsWithStatusZero();?
+        HttpSession session = request.getSession();
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
+        if (customer == null) {
+            // Phim đang chiếu
+            List<Movie> listmovie = (List<Movie>) service.showPhimDangChieu();
+            model.addAttribute("listmovie", listmovie);
+            // Phim sắp chiếu
+            List<Movie> listmovie1 = (List<Movie>) service.showPhimSapChieu();
+            model.addAttribute("listmovie1", listmovie1);
+        } else {
+            List<Movie> listmovie = (List<Movie>) service.showPhimDangChieu();
+            model.addAttribute("listmovie", listmovie);
+            // Phim sắp chiếu
+            List<Movie> listmovie1 = (List<Movie>) service.showPhimSapChieu();
+            model.addAttribute("listmovie1", listmovie1);
+            String soldTicketsCountBill = billRepository.countSoldTicket(customer.getId());
+            model.addAttribute("soldTicketsCountBill", soldTicketsCountBill);
+        }
+
+        return "users/FilmBooking";
+    }
+
+    @GetMapping("/movie/edit/{id}")
+    public String chiTietPhim(@PathVariable("id") String id, Model model) {
+        Movie movie = service.findById(id);
+        model.addAttribute("movie", movie);
+
+        return "users/ChiTietPhim";
+
+    }
+
+
+    @GetMapping("/phimchieu")
+    public String showPhimChieu(Model model, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
+        if (customer == null) {
+            // Phim đang chiếu
+            List<Movie> listmovie = (List<Movie>) service.showPhimDangChieu();
+            model.addAttribute("listmovie", listmovie);
+            // Phim sắp chiếu
+            List<Movie> listmovie1 = (List<Movie>) service.showPhimSapChieu();
+            model.addAttribute("listmovie1", listmovie1);
+        } else {
+            List<Movie> listmovie = (List<Movie>) service.showPhimDangChieu();
+            model.addAttribute("listmovie", listmovie);
+            // Phim sắp chiếu
+            List<Movie> listmovie1 = (List<Movie>) service.showPhimSapChieu();
+            model.addAttribute("listmovie1", listmovie1);
+            String soldTicketsCountBill = billRepository.countSoldTicket(customer.getId());
+            model.addAttribute("soldTicketsCountBill", soldTicketsCountBill);
+        }
+
+        return "users/Phim";
+    }
+
+    @GetMapping("/lichchieu")
+    public String showLichChieu(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession();
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
+
+
+        List<Cinema> listcinema = (List<Cinema>) cinemaService.fillAll();
+        model.addAttribute("listcinema", listcinema);
+
+        List<DtoMovie> listmovie = (List<DtoMovie>) service.showPhishowPhimSapChieuAndDangChieumSapChieu().stream().map(movie -> modelMapper.map(movie, DtoMovie.class)).collect(Collectors.toList());
+        model.addAttribute("listmovie", listmovie);
+        return "users/LichChieu";
+    }
+
+    @GetMapping("/chitietphim")
+    public String showChiTietPhim() {
+
+        return "users/ChiTietPhim";
+    }
+
+
+    @GetMapping("/search/schedule")
+    public String getSchedule(Model model,
+                              @RequestParam(value = "name", required = false) String name,
+                              @DateTimeFormat(pattern = "dd/MM/yyyy") @RequestParam(value = "nameMovie", required = false) String nameMovie,
+                              @RequestParam(value = "startAt", required = false) LocalDate startAt,
+                              @RequestParam(value = "startTime", required = false) Integer startTime,
+                              @RequestParam(value = "endTime", required = false) Integer endTime) {
+        name = Strings.isEmpty(name) ? null : name;
+        nameMovie = Strings.isEmpty(nameMovie) ? null : nameMovie;
+//        if (name != null || nameMovie != null || startAt != null || startTime != null || endTime != null) {
+//            scheduleService.searchSchedule(name, startAt, nameMovie, startTime, endTime);
+//        }
+        List<Cinema> listcinema = (List<Cinema>) cinemaService.fillAll();
+        model.addAttribute("listcinema", listcinema);
+        List<DtoMovie> listmovie = (List<DtoMovie>) service.showPhishowPhimSapChieuAndDangChieumSapChieu().stream().map(movie
+                -> modelMapper.map(movie, DtoMovie.class)).collect(Collectors.toList());
+        model.addAttribute("listmovie", listmovie);
+        List<Schedule> allSuatChieu = repository1.findByConditions(name, startAt, nameMovie, startTime, endTime);
+        Map<String, Map<String, List<LocalDateTime>>> suatChieuMap = new HashMap<>();
+        for (Schedule suatChieu : allSuatChieu) {
+            String tenPhim = suatChieu.getMovie().getName();
+            String theloai = String.valueOf(suatChieu.getMovie().getMovieTypes());
+            Integer thoiluong = suatChieu.getMovie().getMovieDuration();
+            String img = suatChieu.getMovie().getImage();
+            String combinedKey = tenPhim + "_" + theloai + "_" + thoiluong + "_" + img; // Create a combined key by concatenating the two keys
+            String phongChieu = suatChieu.getRoom().getName();
+            String rapchieu = suatChieu.getRoom().getCinema().getName();
+            LocalDateTime gioChieu = suatChieu.getStartAt();
+
+            if (!suatChieuMap.containsKey(combinedKey)) {
+                suatChieuMap.put(combinedKey, new HashMap<>());
+            }
+
+            Map<String, List<LocalDateTime>> phongChieuMap = suatChieuMap.get(combinedKey);
+
+            if (!phongChieuMap.containsKey(phongChieu)) {
+                phongChieuMap.put(phongChieu, new ArrayList<>());
+            }
+
+            List<LocalDateTime> gioChieuList = phongChieuMap.get(phongChieu);
+            gioChieuList.add(gioChieu);
+
+        }
+        for (Map<String, List<LocalDateTime>> phongChieuMap : suatChieuMap.values()) {
+            for (List<LocalDateTime> gioChieuList : phongChieuMap.values()) {
+                Collections.sort(gioChieuList);
+            }
+        }
+        model.addAttribute("suatChieuMap", suatChieuMap);
+        return "users/timkiem";
+    }
+
+    @GetMapping("/rank-and-membership")
+    public String rankAndMembership(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Customer customer = (Customer) session.getAttribute("customer");
+        List<GeneralSetting> generalSettingList = generalSettingRepository.findAll();
+        List<RankCustomer> rankCustomerList = rankCustomerService.fillAll();
+        Collections.sort(rankCustomerList, new RankCustomerComparator());
+        model.addAttribute("customer", customer);
+        model.addAttribute("rankCustomerList", rankCustomerList);
+        model.addAttribute("generalSettingList", generalSettingList);
+        return "users/rankAndMembership";
+    }
+
+}
+
---
+++ src/main/resources/static/admin/js/room.js
@@ -0,0 +1,77 @@
+function validateFormRoom(event) {
+    // Lấy các giá trị từ các trường dữ liệu
+    const quantitys = document.getElementById('quantity').value;
+
+    const quantityError = document.getElementById('quantityErr');
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+
+    // Đặt các biến kiểm tra
+    let isValid = true;
+
+    // Kiểm tra trường rỗng
+    if (quantitys.trim() === '') {
+        quantityError.textContent = 'Số lượng phòng không được để trống !';
+        isValid = false;
+    } else if(isNaN(quantitys) || quantitys <= 0 || quantitys >13) {
+        quantityError.textContent = 'Số lượng phòng phải lớn hơn 0 và nhỏ hơn 13 !';
+        isValid = false;
+    } else {
+        movieDurationError.textContent = '';
+    }
+
+    // Nếu có bất kỳ lỗi nào, ngăn chặn sự kiện mặc định của form
+    if (!isValid) {
+        event.preventDefault(); // This line prevents the default behavior without any visible indication
+    }
+}
+
+function updateFormRoom(event){
+    var nameRoom = document.getElementById('nameRoom').value;
+    var nameRoomErr = document.getElementById('nameRoomErr');
+    var descriptionRoom = document.getElementById('descriptionRoom').value;
+    var descriptionRoomErr = document.getElementById('descriptionRoomErr');
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    var isValid = true;
+    if(nameRoom.trim() === ''){
+        nameRoomErr.textContent = "Tên phòng không được trống !";
+        isValid = false;
+    }else {
+        nameRoomErr.innerText = "";
+    }if(descriptionRoom.trim() === ''){
+        descriptionRoomErr.textContent = "Mô tả trống";
+        isValid = false
+    }else {
+        descriptionRoomErr.innerText = "";
+    }
+    if (!isValid) {
+        event.preventDefault(); // This line prevents the default behavior without any visible indication
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.querySelector('.btn-outline-success');
+    var divToShow = document.getElementById('addSeat');
+
+    button.addEventListener('click', function() {
+        divToShow.style.display = 'block';
+    });
+});
